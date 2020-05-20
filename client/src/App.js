30bbbee6bf3d5d5f3706104d3b62dc3cf65f96d3
apc <alexcurran.va@gmail.com>
2020-05-20 09:48:05 -0500

    Setup basic react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, incidunt!</p>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-const pool = require("./config/db");
 // Import Routes
 const todoRouter = require("./routes/todo-router");
 
